8c13a189406b8176bd7d659bd199580bf037373a
Dark-Code-Academy <darkacademy446@gmail.com>
2026-04-09 19:03:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 
 
-function submit() {
+function n1() {
     var phone_numper = "96566473580";
 
     // 📝 التحقق من حقل الاسم
@@ -54,6 +54,388 @@ function submit() {
     window.open(url, "_blank");
 }
 
+function n2() {
+    var phone_numper = "96566473580";
+
+    // 📝 التحقق من حقل الاسم
+    var nameField = document.getElementById("customer_name");
+    var name = nameField.value;
+    if (!name.trim()) {
+        alert("برجاء ملئ حقل الاسم");
+        nameField.focus();
+        nameField.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+        nameField.style.border = "2px solid red";
+        setTimeout(() => { nameField.style.border = ""; }, 2000);
+        return;
+    }
+    var day = "الاحد";
+    var breakf = document.getElementById("new1-break").value;
+    var lanch = document.getElementById("new1-lanch").value;
+    var dinner = document.getElementById("new1-dinner").value;
+    var snack = document.getElementById("new1-snack").value;
+    var salad = document.getElementById("new1-salad").value;
+
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    // 📦 حفظ في localStorage
+    var order = {
+        day: day,
+        date: dateTime,
+        name: name,
+        breakf: breakf,
+        lanch: lanch,
+        dinner: dinner,
+        snack: snack,
+        salad: salad
+    };
+
+    var orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    // 📤 إرسال واتساب
+    var message =
+        "( يوم الأحد )\n" +
+        "الاسم : " + name + "\n" +
+        "الفطار : " + breakf + "\n" +
+        "الغداء : " + lanch + "\n" +
+        "العشاء : " + dinner + "\n" +
+        "الاسناك : " + snack + "\n" +
+        "السلطة : " + salad;
+
+    var url = "https://wa.me/" + phone_numper + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank").focus();
+}
+function n3() {
+    var phone_numper = "96566473580";
+
+    // 📝 التحقق من حقل الاسم
+    var nameField = document.getElementById("customer_name");
+    var name = nameField.value;
+    if (!name.trim()) {
+        alert("برجاء ملئ حقل الاسم");
+        nameField.focus();
+        nameField.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+        nameField.style.border = "2px solid red";
+        setTimeout(() => { nameField.style.border = ""; }, 2000);
+        return;
+    }
+    var day = "الاثنين";
+
+    var breakf = document.getElementById("new2-break").value;
+    var lanch = document.getElementById("new2-lanch").value;
+    var dinner = document.getElementById("new2-dinner").value;
+    var snack = document.getElementById("new2-snack").value;
+    var salad = document.getElementById("new2-salad").value;
+
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    // 📦 حفظ في localStorage
+    var order = {
+        day: day,
+        date: dateTime,
+        name: name,
+        breakf: breakf,
+        lanch: lanch,
+        dinner: dinner,
+        snack: snack,
+        salad: salad
+    };
+
+    var orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    // 📤 إرسال واتساب
+    var message =
+        "( يوم الاثنين )\n" +
+        "الاسم : " + name + "\n" +
+        "الفطار : " + breakf + "\n" +
+        "الغداء : " + lanch + "\n" +
+        "العشاء : " + dinner + "\n" +
+        "الاسناك : " + snack + "\n" +
+        "السلطة : " + salad;
+
+    var url = "https://wa.me/" + phone_numper + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank").focus();
+}
+
+function n4() {
+    var phone_numper = "96566473580";
+
+    // 📝 التحقق من حقل الاسم
+    var nameField = document.getElementById("customer_name");
+    var name = nameField.value;
+    if (!name.trim()) {
+        alert("برجاء ملئ حقل الاسم");
+        nameField.focus();
+        nameField.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+        nameField.style.border = "2px solid red";
+        setTimeout(() => { nameField.style.border = ""; }, 2000);
+        return;
+    }
+    var day = "الثلاثاء";
+    var breakf = document.getElementById("new3-break").value;
+    var lanch = document.getElementById("new3-lanch").value;
+    var dinner = document.getElementById("new3-dinner").value;
+    var snack = document.getElementById("new3-snack").value;
+    var salad = document.getElementById("new3-salad").value;
+
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    // 📦 حفظ الأوردر
+    var order = {
+        day: day,
+        date: dateTime,
+        name: name,
+        breakf: breakf,
+        lanch: lanch,
+        dinner: dinner,
+        snack: snack,
+        salad: salad
+    };
+
+    var orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    // 📤 رسالة واتساب
+    var message =
+        "( يوم الثلاثاء )\n" +
+        "الاسم : " + name + "\n" +
+        "الفطار : " + breakf + "\n" +
+        "الغداء : " + lanch + "\n" +
+        "العشاء : " + dinner + "\n" +
+        "الاسناك : " + snack + "\n" +
+        "السلطة : " + salad;
+
+    var url = "https://wa.me/" + phone_numper + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank").focus();
+}
+
+function n5() {
+    var phone_numper = "96566473580";
+
+    // 📝 التحقق من حقل الاسم
+    var nameField = document.getElementById("customer_name");
+    var name = nameField.value;
+    if (!name.trim()) {
+        alert("برجاء ملئ حقل الاسم");
+        nameField.focus();
+        nameField.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+        nameField.style.border = "2px solid red";
+        setTimeout(() => { nameField.style.border = ""; }, 2000);
+        return;
+    }
+    var day = "الاربعاء";
+    var breakf = document.getElementById("new4-break").value;
+    var lanch = document.getElementById("new4-lanch").value;
+    var dinner = document.getElementById("new4-dinner").value;
+    var snack = document.getElementById("new4-snack").value;
+    var salad = document.getElementById("new4-salad").value;
+
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    // 📦 حفظ الأوردر
+    var order = {
+        day: day,
+        date: dateTime,
+        name: name,
+        breakf: breakf,
+        lanch: lanch,
+        dinner: dinner,
+        snack: snack,
+        salad: salad
+    };
+
+    var orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    // 📤 رسالة واتساب
+    var message =
+        "( يوم الاربعاء )\n" +
+        "الاسم : " + name + "\n" +
+        "الفطار : " + breakf + "\n" +
+        "الغداء : " + lanch + "\n" +
+        "العشاء : " + dinner + "\n" +
+        "الاسناك : " + snack + "\n" +
+        "السلطة : " + salad;
+
+    var url = "https://wa.me/" + phone_numper + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank").focus();
+}
+
+function n6() {
+    var phone_numper = "96566473580"; // رقم الهاتف بدون +
+
+    // 📝 جمع بيانات العميل
+    var nameField = document.getElementById("customer_name");
+    var name = nameField.value;
+    if (!name.trim()) { // تحقق إن الاسم مش فاضي
+        alert("برجاء ملئ حقل الاسم");
+
+        // نركز على الحقل
+        nameField.focus();
+
+        // إذا كان داخل scrollable div
+        nameField.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+
+        // نعطيه حدود قصيرة ك highlight مؤقت
+        nameField.style.border = "2px solid red";
+        setTimeout(() => { nameField.style.border = ""; }, 2000);
+
+        return;
+    }
+    var day = "الخميس";
+    var breakf = document.getElementById("new5-break").value;
+    var lanch = document.getElementById("new5-lanch").value;
+    var dinner = document.getElementById("new5-dinner").value;
+    var snack = document.getElementById("new5-snack").value;
+    var salad = document.getElementById("new5-salad").value;
+
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    var order = {
+        date: day,
+        name: name,
+        breakf: breakf,
+        lanch: lanch,
+        dinner: dinner,
+        snack: snack,
+        salad: salad
+    };
+
+    var orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    var message =
+        "( يوم الخميس )\n" +
+        "الاسم : " + name + "\n" +
+        "الفطار : " + breakf + "\n" +
+        "الغداء : " + lanch + "\n" +
+        "العشاء : " + dinner + "\n" +
+        "الاسناك : " + snack + "\n" +
+        "السلطة : " + salad;
+
+    var url = "https://wa.me/" + phone_numper + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank");
+}
+
+function n7() {
+    var phone_numper = "96566473580";
+
+    // 📝 التحقق من حقل الاسم
+    var nameField = document.getElementById("customer_name");
+    var name = nameField.value;
+    if (!name.trim()) {
+        alert("برجاء ملئ حقل الاسم");
+        nameField.focus();
+        nameField.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+        nameField.style.border = "2px solid red";
+        setTimeout(() => { nameField.style.border = ""; }, 2000);
+        return;
+    }
+    var day = "الجمعة";
+
+    var breakf = document.getElementById("new6-break").value;
+    var lanch = document.getElementById("new6-lanch").value;
+    var dinner = document.getElementById("new6-dinner").value;
+    var snack = document.getElementById("new6-snack").value;
+    var salad = document.getElementById("new6-salad").value;
+
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    // 📦 حفظ الأوردر
+    var order = {
+        day: day,
+        date: dateTime,
+        name: name,
+        breakf: breakf,
+        lanch: lanch,
+        dinner: dinner,
+        snack: snack,
+        salad: salad
+    };
+
+    var orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    // 📤 رسالة واتساب
+    var message =
+        "( يوم الجمعة )\n" +
+        "الاسم : " + name + "\n" +
+        "الفطار : " + breakf + "\n" +
+        "الغداء : " + lanch + "\n" +
+        "العشاء : " + dinner + "\n" +
+        "الاسناك : " + snack + "\n" +
+        "السلطة : " + salad;
+
+    var url = "https://wa.me/" + phone_numper + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank").focus();
+}
+
+
+
+function submit() {
+    var phone_numper = "96566473580";
+
+    // 📝 التحقق من حقل الاسم
+    var nameField = document.getElementById("customer_name");
+    var name = nameField.value;
+    if (!name.trim()) {
+        alert("برجاء ملئ حقل الاسم");
+        nameField.focus();
+        nameField.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+        nameField.style.border = "2px solid red";
+        setTimeout(() => { nameField.style.border = ""; }, 2000);
+        return;
+    }
+    var day = "السبت";
+    var breakf = document.getElementById("break").value;
+    var lanch = document.getElementById("lanch").value;
+    var dinner = document.getElementById("dinner").value;
+    var snack = document.getElementById("snack").value;
+    var salad = document.getElementById("salad").value;
+
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    var order = {
+        day : day,
+        date: dateTime,
+        name: name,
+        breakf: breakf,
+        lanch: lanch,
+        dinner: dinner,
+        snack: snack,
+        salad: salad
+    };
+
+    var orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+
+    var message =
+        "( يوم السبت )\n" +
+        "الاسم : " + name + "\n" +
+        "الفطار : " + breakf + "\n" +
+        "الغداء : " + lanch + "\n" +
+        "العشاء : " + dinner + "\n" +
+        "الاسناك : " + snack + "\n" +
+        "السلطة : " + salad;
+
+    var url = "https://wa.me/" + phone_numper + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank");
+}
 function submit_new1() {
     var phone_numper = "96566473580";
 
